9538fb5c02f44651a7a3b73eb6d52e53cbfd933e
domin <dominideliyak@gmail.com>
2021-06-23 13:03:17 +0200

    added a program to count elements of an array which are divided by 3 and 5

---
+++ arrayCount.py
@@ -0,0 +1,10 @@
+arr = [2,3,5,10,6,15]
+count_num = 0
+
+for x in arr:
+    if x % 3 == 0:
+        count_num = count_num + 1
+    elif x % 5 == 0:
+        count_num = count_num + 1
+
+print(count_num)
